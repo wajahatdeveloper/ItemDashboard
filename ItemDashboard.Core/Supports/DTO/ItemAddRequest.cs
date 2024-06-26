c0e7bfa9ad45119aecc48267445b68f259b0f6af
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2024-06-26 11:50:47 +0500

    Add DTO's for Core Access Layer

---
+++ ItemDashboard.Core/Supports/DTO/ItemAddRequest.cs
@@ -0,0 +1,31 @@
+﻿using ItemDashboard.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemDashboard.Core.Supports.DTO;
+
+/// <summary>
+/// Acts as a DTO for inserting a new person
+/// Validates data before insertion to ensure a clean domain
+/// </summary>
+public class ItemAddRequest
+{
+    [Required(ErrorMessage = "Item Name cannot be blank")]
+    public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "Item Description cannot be blank")]
+    public string Description { get; set; } = "";
+
+    /// <summary>
+    /// Converts the current DTO Object to Domain Object
+    /// </summary>
+    /// <returns></returns>
+    public Item ToItem()
+    {
+        return new Item { Name = Name, Description = Description };
+    }
+}
---
+++ ItemDashboard.Core/Supports/DTO/ItemResponse.cs
@@ -0,0 +1,59 @@
+﻿using ItemDashboard.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ItemDashboard.Core.Supports.DTO;
+
+/// <summary>
+/// Acts as a DTO used as return type from domain
+/// </summary>
+public class ItemResponse : IEquatable<ItemResponse>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public DateTime CreatedBy { get; set; }
+    public DateTime UpdatedBy { get; set; }
+
+    public bool Equals(ItemResponse? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Id == other.Id && Name == other.Name && Description == other.Description;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+
+public static class ItemExtensions
+{
+    public static ItemResponse ToItemResponse(this Item item)
+    {
+        return new ItemResponse()
+        {
+            Id = item.Id,
+
+            Name = item.Name,
+            Description = item.Description,
+
+            CreatedDate = item.CreatedDate,
+            CreatedBy = item.CreatedBy,
+            UpdatedBy = item.UpdatedBy,
+            UpdatedDate = item.UpdatedDate,
+        };
+    }
+}
