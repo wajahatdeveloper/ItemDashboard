5c9dc4fa074883cdd0403871ba286c45b43e313a
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2024-06-26 11:25:41 +0500

    Add Item Domain Entity and Repo Interface

---
+++ ItemDashboard.Core/Domain/Entities/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace ItemDashboard.Core.Domain.Entities;
+
+/// <summary>
+/// Item Domain Model Class
+/// </summary>
+public class Item
+{
+    /// <summary>
+    /// INFRASTRUCTURE DATA
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// MAIN DATA
+    /// </summary>
+    [Required]
+    [StringLength(40)] //nvarchar(40)
+    public string Name { get; set; } = "";
+    [Required]
+    [StringLength(200)] //nvarchar(200)
+    public string Description { get; set; } = "";
+    
+    /// <summary>
+    /// TRACKING DATA
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public DateTime CreatedBy { get; set; }
+    public DateTime UpdatedBy { get; set; }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
---
+++ ItemDashboard.Core/Domain/RepositoryContracts/IItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using ItemDashboard.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemDashboard.Core.Domain.RepositoryContracts;
+
+/// <summary>
+/// Represents data access interface for managing item entity
+/// </summary>
+public interface IItemRepository
+{
+    /// <summary>
+    /// Basic CRUD Methods
+    /// </summary>
+    Task<Item> AddItem(Item item);
+    Task<Item> UpdateItem(Item item);
+    Task DeleteItemById(int id);
+    Task<Item> GetItemById(int id);
+
+    /// <summary>
+    /// Advanced CRUD Methods
+    /// </summary>
+    Task<IEnumerable<Item>> GetAllItems();
+}
