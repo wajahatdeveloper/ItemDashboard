5a9764001ddad1f0ac466fb9db5b12bf86aa976e
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2024-07-02 15:47:18 +0500

    Add POS Area for Point of Sale

---
+++ ItemDashboard.UI/Areas/POS/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItemDashboard.UI.Areas.POS.Controllers;
+
+[Area("POS")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        _logger.LogInformation("Hello from POS!");
+
+        return View();
+    }
+}
